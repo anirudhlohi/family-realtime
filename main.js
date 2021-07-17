d6aaff7ff2b68144c1d835d3bcbc9404b6a3d1f9
anirudhlohi <74246267+anirudhlohi@users.noreply.github.com>
2021-07-17 19:16:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/kYQNO9v8E/model.json",modelLoaded);
   }
   function draw(){
-  
+     image(video,0,0,300,300);
+     classifier.classify(video,gotResult);
   }
+  function modelLoaded(){
+    console.log("Model Is Loaded");
+  }
+  function gotResult(error,result){
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(result);
+    document.getElementById("object_name").innerHTML = result[0].label;
+    document.getElementById("object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+  }
+  }
+  
+  
